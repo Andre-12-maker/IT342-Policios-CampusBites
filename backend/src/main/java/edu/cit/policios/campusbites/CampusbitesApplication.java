5752968f611013964c79825a15a780e62ab9eecf
LAB13_USER ON WS11 <L13Y09W11@LabHCI.net>
2026-03-07 08:17:20 +0800

    Initial commit of backend files

---
+++ backend/src/main/java/edu/cit/policios/campusbites/CampusbitesApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.policios.campusbites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusbitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusbitesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/policios/campusbites/CampusbitesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.policios.campusbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusbitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
